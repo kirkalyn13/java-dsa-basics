173ae9d4d0060659fedcd02c527af1c66b101672
Kirk Santos <kirkalyn13@gmail.com>
2024-09-13 13:58:54 +0800

    feat: binary search tree

---
+++ src/main/java/algorithms/tree/TraverseBinarySearchTree.java
@@ -0,0 +1,19 @@
+package algorithms.tree;
+
+public class TraverseBinarySearchTree {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        tree.insert(new Node(5));
+        tree.insert(new Node(1));
+        tree.insert(new Node(9));
+        tree.insert(new Node(2));
+        tree.insert(new Node(7));
+        tree.insert(new Node(3));
+        tree.insert(new Node(6));
+        tree.insert(new Node(4));
+        tree.insert(new Node(8));
+
+        tree.display();
+    }
+}
